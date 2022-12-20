6343756d2996f4104462f396e8914d2f9336a531
James Parrish <jthomasparrishjr@gmail.com>
2022-12-19 18:11:52 -0600

    commit tree 789e5139efcc5690a0c1acbfe3307eae92b7e071

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,30 @@
 class ApplicationController < Sinatra::Base
-
+set :default_content_type, 'application/json'
   # add routes
+  get '/bakeries' do
+    # get all the bakeries from the database
+    bakeries = Bakery.all
+    # send them back as a JSON array
+    bakeries.to_json
+  end
+
+  get '/bakeries/:id' do
+    # look up bakery in database by id
+    bakery = Bakery.find(params[:id])
+    # send bakery back along with its baked goods as a JSON array
+    bakery.to_json(include: :baked_goods)
+  end
+
+  get '/baked_goods/by_price' do
+    #look up baked goods by price in database
+    baked_goods = BakedGood.order(price: :desc)
+    # send info sorted by price in descending order
+    baked_goods.to_json
+  end
+
+  get '/baked_goods/most_expensive' do
+    baked_good = BakedGood.order(price: :desc).first
+    baked_good.to_json
+  end
 
 end
--- app/models/baked_good.rb
+++ app/models/baked_good.rb
@@ -1,3 +1,6 @@
 class BakedGood < ActiveRecord::Base
   # add association macro here
+  belongs_to :bakery
+
+
 end
